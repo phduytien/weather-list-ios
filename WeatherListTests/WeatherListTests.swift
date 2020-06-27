fecb75855b7c3d2498a775b5ef4905e3dfdc44fd
TienPham <tienpham@nvg.vn>
2020-06-27 10:20:46 +0700

    Initial Xcode project

---
+++ WeatherListTests/WeatherListTests.swift
@@ -0,0 +1,34 @@
+//
+//  WeatherListTests.swift
+//  WeatherListTests
+//
+//  Created by TienPham on 6/27/20.
+//  Copyright © 2020 Tien Pham. All rights reserved.
+//
+
+import XCTest
+@testable import WeatherList
+
+class WeatherListTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
